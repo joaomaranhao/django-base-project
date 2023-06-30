59125223b6d3067b1cef73f56eeafee6737442d2
João Maranhão <joaofelipemaranhao@gmail.com>
2023-06-30 10:29:51 -0300

    chore: change project layout

--- app/manage.py
+++ app/manage.py
@@ -2,12 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
 from dotenv import load_dotenv
+
 
 def main():
     """Run administrative tasks."""
     load_dotenv()
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
